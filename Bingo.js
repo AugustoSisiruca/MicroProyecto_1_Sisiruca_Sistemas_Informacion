4438d93ec5b5e20140bb14e3fbc858d686a475dd
AugustoSisiruca <sisiruca.augusto@correo.unimet.edu.ve>
2024-02-11 23:35:33 -0400

    Ultimos cambios 😞

--- Bingo.js
+++ Bingo.js
@@ -1,3 +1,9 @@
+//Funcion que verifica que exita el numero marcado
+function esNumeroMarcado(numero) {
+    let numerosMarcados = obtenerNumeroBingo();
+    return numerosMarcados.includes(numero);
+}
+
 // Función para agregar un nombre de jugador a la lista en LocalStorage
 function agregarJugador(nombre) {
     let jugadores = obtenerJugadores();
@@ -20,20 +26,76 @@ function obtenerTamanoCarton() {
     return localStorage.getItem('tamanoCarton');
 }
 
+// Función para obtener la lista de jugadores almacenada en LocalStorage
+function obtenerCartonJugador() {
+    let listaCartonJugadores = localStorage.getItem('listaCartonBingo');
+    return listaCartonJugadores ? JSON.parse(listaCartonJugadores) : [];
+}
+
+
+// Función para agregar el carton de Bingo de un jugador a la lista en LocalStorage
+function agregarCartonJugador(listaCarton) {
+    let listaCartonJugadores = obtenerCartonJugador();
+    listaCartonJugadores.push(listaCarton);
+    localStorage.setItem('listaCartonBingo', JSON.stringify(listaCartonJugadores));
+}
+
 
 // Función para obtener los numeros que se desean colorear en el cartón desde el almacenamiento local
-function obtenerNumerosMarcados() {
+function obtenerNumeroBingo() {
     let numerosMarcados = localStorage.getItem('numerosMarcados');
     return numerosMarcados ? JSON.parse(numerosMarcados) : [];
 }
 
 /// Función para guardar los numeros del bingo en el almacenamiento local
-function marcarNumero(numero) {
-    let numerosMarcados = obtenerNumerosMarcados();
+function agregarNumeroBingo(numero) {
+    let numerosMarcados = obtenerNumeroBingo();
     numerosMarcados.push(numero);
     localStorage.setItem('numerosMarcados', JSON.stringify(numerosMarcados));
 }
 
+class Jugador {
+    constructor(nombre, matriz, puntos) {
+        this.nombre = nombre;
+        this.matriz = matriz;
+        this.puntos = puntos;
+
+    }
+}
+
+//Funcion que manda parametros a la funcion verificarLineas y a verificarCartonCompleto
+function verificarCartonPuntos() {
+    let nombre = obtenerJugadores();
+    let matrizCodigo = [];
+    let setNombre = new Set(nombre); 
+    let listaNombre = Array.from(setNombre);
+    for (let k = 0; k < listaNombre.length; k++) {
+        matrizCodigo.push(listaNombre[k])
+    }
+    let matriz = obtenerCartonJugador();
+    let puntos = 0;
+    let jugadores = [];
+
+    for (let i = 0; i < listaNombre.length; i++) {
+        let matriz = obtenerCartonJugador();
+        let puntos = 0;
+    
+        // Obtener el primer elemento de cada matriz en la lista de matrices
+        let primerElementoMatriz = [];
+        for (let j = 0; j < matriz.length; j++) {
+            primerElementoMatriz.push(matriz[j][0]);
+        }
+        console.log(primerElementoMatriz)
+        let jugador = new Jugador(matrizCodigo[i], primerElementoMatriz, puntos);
+        jugadores.push(jugador);
+    }
+
+    verificarLineas(matriz, listaNombre);
+    verificarCartonCompleto(matriz);
+    
+}
+
+//Funcion que comprueba los datos de los usuarios que se registren
 function guardarDatos() {
     //Obtener los inputs que piden
     let jugador1 = document.getElementById("jugador1").value;
@@ -47,6 +109,12 @@ function guardarDatos() {
         alert("Por favor, complete todos los campos.");
         return; // Detener la ejecución si hay campos vacíos
     }
+
+    if (jugador1 === jugador2 || jugador1 === jugador3 || jugador1 === jugador4 || jugador2 === jugador3 || jugador2 === jugador4 || jugador3 === jugador4){
+        alert("Por favor, ponga nombres distintos.");
+        return; // Detener la ejecución si hay campos con nombres iguales
+    }
+    
     // Validar que el tamaño sea un número válido
     largoCarton = parseInt(largoCarton);
     if(Number.isInteger(largoCarton) && largoCarton >= 3 && largoCarton <= 5) {
@@ -65,55 +133,32 @@ function guardarDatos() {
     }   
 }
 
-
-
-// Función para crear y mostrar el cartón de bingo de un jugador
-function crearCartonJugador(idContenedor, numeroAleatorio) {
-    let cartonJugadorBingo = document.getElementById(idContenedor);
-    let tamanoCarton = obtenerTamanoCarton();
-    let carton = crearCartonBingo(tamanoCarton);
-    
-    // Limpiar el contenedor antes de agregar nuevos números
-    cartonJugadorBingo.innerHTML = "";
-    
-    // Mostrar el cartón de Bingo en el contenedor
-    carton.forEach((fila, i) => {
-        let filaDiv = document.createElement('div');
-        filaDiv.classList.add('filaBingo');
-
-        fila.forEach((numero, j) => {
-            let numeroDiv = document.createElement('div');
-            numeroDiv.classList.add('numeroBingo');
-            numeroDiv.textContent = numero;
-
-            // Si el número coincide con el número aleatorio generado en el turno
-            // o si el número ya está marcado en el cartón, resaltar la casilla en rojo
-            if (numero === numeroAleatorio || esNumeroMarcado(numero, carton, i, j)) {
-                numeroDiv.classList.add('marcado');
-            }
-
-            filaDiv.appendChild(numeroDiv);
-        });
-
-        cartonJugadorBingo.appendChild(filaDiv);
+// Función para mostrar el cartón de bingo de un jugador
+function crearCartonJugadorBingo(idContenedor, numeroAleatorio) {
+    let cartonJugador = document.getElementById(idContenedor); // Obtener el contenedor del cartón del jugador
+    let numerosCarton = cartonJugador.querySelectorAll('.numeroBingo'); // Obtener todos los elementos de número en el cartón
+    agregarNumeroBingo(numeroAleatorio); //Guardar numero 
+    // Recorrer todos los elementos de número en el cartón
+    numerosCarton.forEach(numero => {
+        // Verificar si el número coincide con el número aleatorio generado
+        if (parseInt(numero.textContent) === numeroAleatorio) {
+            numero.classList.add('marcado'); // Agregar la clase 'marcado' al número
+        }
     });
 }
 
-// Función para verificar si un número en el cartón ya ha sido marcado
-function esNumeroMarcado(numero, carton, filaIndex, columnaIndex) {
-    for (let i = 0; i < carton.length; i++) {
-        for (let j = 0; j < carton[i].length; j++) {
-            if (i === filaIndex && j === columnaIndex) continue; // Saltar la casilla actual
-            if (carton[i][j] === numero) return true; // Si el número está en otra casilla, está marcado
-        }
+// Función para crear el cartón de bingo de un jugador
+function crearCartonJugador(idContenedor) {
+    let tamanoCarton = obtenerTamanoCarton();
+    let matrizJugadorBingo = crearCartonBingo(tamanoCarton, idContenedor);
+    agregarCartonJugador(matrizJugadorBingo);
     }
-    return false;
-}
 
 
-function crearCartonBingo(tamanio) {
+// Función para crear el cartón de bingo
+function crearCartonBingo(tamanio, idContenedor) {
+    let cartonJugadorBingo = document.getElementById(idContenedor);
     let matrizCarton = []; //Crear matriz
-    let numeroBingo = 0; //Crear numero
     let numeroRepetido = []; //Verificar numero repetido
 
     //El for crea una matriz vacía de tamaño N*N 
@@ -123,40 +168,77 @@ function crearCartonBingo(tamanio) {
     //Estos for llenan la matriz con numeros aleratorios del 1 al 50
     for (let i = 0; i < tamanio; i++) {
         for (let j = 0; j < tamanio; j++) {
+            let numeroBingo;
             do {
-                numeroBingo = Math.floor(Math.random() * 50) + 1 //Genera un numero random entre 1 y 50 se suma 1 para asegurar que se llegue a [1,50] y no quede en [0,49]
+                numeroBingo = Math.floor(Math.random() * 50) + 1; //Genera un numero random entre 1 y 50 se suma 1 para asegurar que se llegue a [1,50] y no quede en [0,49]
             } while (numeroRepetido.includes(numeroBingo));
 
             numeroRepetido.push(numeroBingo); //Agrega el numero a la lista de numeros repetidos
-            matrizCarton[i][j]= numeroBingo;
+            matrizCarton[i][j] = numeroBingo;
         }
     }
+
+    // Crear los elementos div para cada número en la matriz
+    let cartonDiv = document.createElement('div');
+    cartonDiv.classList.add('cartonBingo');
+
+    // Recorrer la matriz y crear elementos div para cada número
+    for (let i = 0; i < matrizCarton.length; i++) {
+        let filaDiv = document.createElement('div');
+        filaDiv.classList.add('filaBingo');
+
+        for (let j = 0; j < matrizCarton[i].length; j++) {
+            let numeroDiv = document.createElement('div');
+            numeroDiv.classList.add('numeroBingo');
+            numeroDiv.textContent = matrizCarton[i][j];
+            filaDiv.appendChild(numeroDiv);
+        }
+        cartonDiv.appendChild(filaDiv);
+    }
+
+    // Agregar el cartón al contenedor en el HTML
+
+    cartonJugadorBingo.appendChild(cartonDiv);
+    // Retornar la matriz generada
     return matrizCarton;
 }
 
-function verificarLinea(carton) {
+//Funcion pra verificar si hay marcas de puntos de lineas rojas
+function verificarLineas(carton, jugador) {
+    let puntos = 0;
     // Verificar líneas horizontales
-    for (let i = 0; i < carton.length; i++) {
-        let lineaCompleta = true;
-        for (let j = 0; j < carton[i].length; j++) {
-            if (!esNumeroMarcado(carton[i][j])) {
-                lineaCompleta = false;
-                break;
+    for (let k = 0; k < carton.length; k++) {
+        for (let i = 0; i < carton[k].length; i++) {
+            let lineaCompleta = true;
+            for (let j = 0; j < carton[k][i].length; j++) {
+                if (!esNumeroMarcado(carton[k][i][j])) {
+                    lineaCompleta = false;
+                    break;
+                }
+            }
+            if (lineaCompleta) {
+                puntos++;
+                console.log(`${jugador[k]} ha completado una línea horizontal.`);
             }
         }
-        if (lineaCompleta) return true;
     }
 
+
     // Verificar líneas verticales
-    for (let i = 0; i < carton[0].length; i++) {
-        let lineaCompleta = true;
-        for (let j = 0; j < carton.length; j++) {
-            if (!esNumeroMarcado(carton[j][i])) {
-                lineaCompleta = false;
-                break;
+    for (let k = 0; k < carton.length; k++) {
+        for (let i = 0; i < carton[k].length; i++) {
+            let lineaCompleta = true;
+            for (let j = 0; j < carton[k].length; j++) {
+                if (!esNumeroMarcado(carton[k][j][i])) {
+                    lineaCompleta = false;
+                    break;
+                }
+            }
+            if (lineaCompleta) {
+                puntos++;
+                console.log(`${jugador[k]} ha completado una línea vertical.`);
             }
         }
-        if (lineaCompleta) return true;
     }
 
     // Verificar línea diagonal principal
@@ -167,7 +249,10 @@ function verificarLinea(carton) {
             break;
         }
     }
-    if (diagonalPrincipalCompleta) return true;
+    if (diagonalPrincipalCompleta) {
+        puntos++;
+        console.log(`${jugador} ha completado la línea diagonal principal.`);
+    }
 
     // Verificar línea diagonal secundaria
     let diagonalSecundariaCompleta = true;
@@ -177,7 +262,25 @@ function verificarLinea(carton) {
             break;
         }
     }
-    if (diagonalSecundariaCompleta) return true;
+    if (diagonalSecundariaCompleta) {
+        puntos++;
+        console.log(`${jugador} ha completado la línea diagonal secundaria.`);
+    }
 
-    return false;
+    return puntos;
+}
+
+
+// Función para verificar si un cartón está completo
+function verificarCartonCompleto(matriz) {
+    for (let k = 0; k < matriz.length; k++) {
+        for (let i = 0; i < matriz[k].length; i++) {
+            for (let j = 0; j < matriz[k][i].length; j++) {
+                if (!esNumeroMarcado(matriz[k][i][j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- Contador.js
+++ Contador.js
@@ -1,3 +1,31 @@
+const sections = document.querySelectorAll('.section');
+let currentSectionIndex = 0;
+
+function showSection(index) {
+    sections.forEach((section, i) => {
+        if (i === index) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    });
+}
+
+showSection(currentSectionIndex);
+
+document.getElementById('leftArrow').addEventListener('click', () => {
+    currentSectionIndex = (currentSectionIndex === 0) ? sections.length - 1 : currentSectionIndex - 1;
+    showSection(currentSectionIndex);
+});
+
+document.getElementById('rightArrow').addEventListener('click', () => {
+    currentSectionIndex = (currentSectionIndex === sections.length - 1) ? 0 : currentSectionIndex + 1;
+    showSection(currentSectionIndex);
+});
+
+
+
+
 // Obtener referencia al contador de turnos y al botón de siguiente turno
 const turnCounter = document.getElementById('contadorTurno');
 const nextTurnBtn = document.getElementById('siguienteTurno');
@@ -7,37 +35,36 @@ const  bingoNumber = document.getElementById('numeroBingo');
 let turnCount = 0;
 let listaConteo = [];
 let numeroBingo = 0;
-let guardarNumero = [];
 
-// Función para manejar el clic en el botón de siguiente turno
 nextTurnBtn.addEventListener('click', () => {
     if (turnCount == 25){
+        verificarCartonPuntos(); // Verificar si se completó algún cartón y asignar puntos
         window.location.href="Terminar.html";
     }
     // Incrementar el contador de turnos
     turnCount++;
-
     // Generar un número aleatorio
     let numeroAleatorio;
     do {
         numeroAleatorio = Math.floor(Math.random() * 50) + 1; //Genera un numero random entre 1 y 50 se suma 1 para asegurar que se llegue a [1,50] y no quede en [0,49]
     } while (listaConteo.includes(numeroAleatorio));
     listaConteo.push(numeroAleatorio);
-
+    localStorage.getItem('NumeroRandom', numeroAleatorio);
     // Actualizar el texto del contador de turnos en el HTML
     bingoNumber.textContent = `Número de Bingo: ${numeroAleatorio}`;
     turnCounter.textContent = `Turno: ${turnCount}`;
-
     // Actualizar los cartones de los jugadores con el número aleatorio generado
-    crearCartonJugador("cartonBingo1", numeroAleatorio);
-    crearCartonJugador("cartonBingo2", numeroAleatorio);
-    crearCartonJugador("cartonBingo3", numeroAleatorio);
-    crearCartonJugador("cartonBingo4", numeroAleatorio);
+    crearCartonJugadorBingo("cartonBingo1", numeroAleatorio);
+    crearCartonJugadorBingo("cartonBingo2", numeroAleatorio);
+    crearCartonJugadorBingo("cartonBingo3", numeroAleatorio);
+    crearCartonJugadorBingo("cartonBingo4", numeroAleatorio);
 });
+
 
 
 function reiniciarCarton() {
     localStorage.removeItem('numerosMarcados'); // Limpiar los números marcados en el almacenamiento local
+    localStorage.removeItem('listaCartonBingo');
 }
 
 function reiniciarJuego() {
@@ -50,6 +77,8 @@ function reiniciarJuego() {
 function salirJuego() {
     if (confirm("¿Estás seguro de que deseas salir del juego?")) {
         reiniciarCarton(); // Llamar a la función para reiniciar el estado del cartón del bingo
+        localStorage.removeItem('listaJugadores');
+        localStorage.removeItem('tamanoCarton');
         window.location.href="MenuPrincipal.html"; //Vuelve al menu principal
     }
 }
